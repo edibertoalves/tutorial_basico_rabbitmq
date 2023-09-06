79b14a95225022bfcc84488a82045e35d61b0685
Ediberto Alves <ediberto@gmail.com>
2023-09-06 17:36:12 -0300

    primeiro commit

---
+++ Basico/1/Consumer/Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare(
+            queue: "saudacao_1",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($"[x] Recebida: {message}");
+        };
+
+        channel.BasicConsume(queue: "saudacao_1", autoAck: true, consumer: consumer);
+
+        Console.ReadLine();
+
+    }
+}
+
+
